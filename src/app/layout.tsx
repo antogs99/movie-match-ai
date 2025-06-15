744dfc59432971f07b6f2350d13dfde1c4a9dc93
Antonio Gutierrez <antogs@Antonio-GS-Macbook-Pro.local>
2025-06-14 21:07:44 -0400

    fixing gtag error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AnalyticsWrapper } from "@/lib/gtag";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -56,7 +57,9 @@ export default function RootLayout({
       >
         <>
           {children}
-          <AnalyticsWrapper />
+          <Suspense fallback={null}>
+            <AnalyticsWrapper />
+          </Suspense>
         </>
       </body>
     </html>
